b8717142ea31a33a492eea8a59db4ca051983bf6
prathmesh49 <prathmesh_kurane@yahoo.com>
2023-09-08 13:03:09 +0530

    project setup

---
+++ onlineParkingSystem/onlineParkingSystem/src/test/java/onlineParkingSystem/OnlineParkingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package onlineParkingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineParkingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
